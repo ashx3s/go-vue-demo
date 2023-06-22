ddd2d02873eb50ab43a6c81a0d91f7ef2129fb4f
lilyx13 <ashlyn@lilyx.io>
2023-06-22 00:38:42 -0600

    web server configured hello world example
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- main/server.go
+++ main/server.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", homePageHandler)	
+
+	fmt.Println("Server listening on port 3000")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
+
+// home page handler function
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "hello world!")
+	checkError(err)
+}
+
+// error handle function
+func checkError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
